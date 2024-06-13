3409eb3537ccc013d91859bd35731821001fa92c
Ankita <ankitakasibatla@gmail.com>
2024-06-13 08:02:11 +0530

    problems

--- src/MissingandDup.java
+++ src/MissingandDup.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class MissingandDup {
+public class MissingandDup {
+//    public static void main(String[] args) {
+//        int[] a1 = {  3, 4, 7,  6, 7};
+//
+//        missing(a1);
+//    }
+//    public static void missing(int[] a){
+//        for(int i=0; i<a.length; i++){
+//            if(a[i] != i+1){
+//                int missing = i + 1;
+//                int dup = a[i];
+//                System.out.println(missing);
+//                System.out.println(dup);
+//            }
+//        }
+//    }
+//}
+
+//public class MissingandDup {
+//    public static void main(String[] args) {
+//        int[] a1 = {3, 4, 7, 6, 7};
+//
+//        int[] result = findMissingAndDuplicate(a1);
+//        System.out.println("Duplicate: " + result[0]);
+//        System.out.println("Missing: " + result[1]);
+//    }
+//
+//    public static int[] findMissingAndDuplicate(int[] a) {
+//        int n = a.length;
+//        int duplicate = -1, missing = -1;
+//
+//        // Traverse the array
+//        for (int i = 0; i < n; i++) {
+//            int absValue = Math.abs(a[i]);
+//
+//            // If the value at the index corresponding to the absolute value is negative, it's a duplicate
+//            if (a[absValue - 1] < 0) {
+//                duplicate = absValue;
+//            } else {
+//                // Mark the value at that index as visited by making it negative
+//                a[absValue - 1] = -a[absValue - 1];
+//            }
+//        }
+//
+//        // Traverse the array again to find the missing number
+//        for (int i = 0; i < n; i++) {
+//            if (a[i] > 0) {
+//                missing = i + 1;
+//                break;
+//            }
+//        }
+//
+//        return new int[]{duplicate, missing};
+//    }
 }
--- src/maxiOne.java
+++ src/maxiOne.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class maxiOne {
+public class maxiOne {
+    public static void main(String[] args) {
+
+        int[] a1 = {1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0 ,1 , 1, 1, 0, 1};
+
+        System.out.println(greatones(a1));
+    }
+    public static int greatones(int[] a){
+        int c = 0;
+        int m = 0;
+        for(int i = 0; i<a.length; i++){
+            if(a[i] == 1){
+                c++;
+            }else{
+                c=0;
+            }
+            m = Math.max(m, c);
+        }
+        return m;
+    }
 }
