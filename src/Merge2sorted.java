a0f65c8fb14e8131139b688e46cf2a1699677054
Ankita <ankitakasibatla@gmail.com>
2024-06-14 17:12:28 +0530

    array sorting

--- src/Merge2sorted.java
+++ src/Merge2sorted.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Merge2sorted {
+public class Merge2sorted {
+    public static void main(String[] args) {
+        int[] a1 = {5, 8, 9,};
+        int[] a2 = {4, 7, 8};
+        int n1 = 3;
+        int n2 = 3;
+        int[] a3 = new int[n1+n2];
+        merge(a1, a2, n1, n2, a3);
+    }
+    public static void merge(int[] a1, int[] a2, int n1, int n2, int[] a3){
+        int i=0,j=0,k=0;
+        while(i<n1 && j<n2){
+            if(a1[i] < a2[j]){
+                a3[k] = a1[i];
+                k++;
+                i++;
+            }else{
+                a3[k] = a2[j];
+                k++;
+                j++;
+            }
+        }
+        while(i<n1){
+            a3[k] = a1[i];
+            k++;
+            i++;
+        }
+        while(j<n2){
+            a3[k] = a2[j];
+            k++;
+            j++;
+        }
+        for(int l=0; l<a3.length;l++){
+            System.out.print(a3[l]);
+        }
+    }
 }
--- src/MinOpSortArr.java
+++ src/MinOpSortArr.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MinOpSortArr {
+public class MinOpSortArr {
+    public static void main(String[] args) {
+        int[] a = {1, 5, 3, 4, 2};
+        System.out.println(miniop(a));
+    }
+    public static int miniop(int[] a){
+        int c = 0;
+        for(int i=1; i<a.length; i++){
+            if(a[i] <= a[i-1]){
+                int ch = a[i-1] - a[i] + 1;
+                c += ch;
+                a[i] += ch;
+            }
+        }
+        return c;
+    }
 }
