732f298a60b27d7550dd44e10e3570c74e0366cd
Ankita <ankitakasibatla@gmail.com>
2024-06-18 23:12:27 +0530

    array problems set

--- src/SmallestPosMissing.java
+++ src/SmallestPosMissing.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class SmallestPosMissing {
+public class SmallestPosMissing {
+    public static void main(String[] args) {
+        int[] a = {0, 9, -1, 3, -4, 5};
+        System.out.println(smallpos(a));
+    }
+    public static int smallpos(int[] a){
+        int n = 1000002;
+        boolean[] ans = new boolean[n];
+        for(int i=0; i<n; i++){
+            ans[i] = false;
+        }
+        for(int i=0; i<a.length; i++){
+            if(a[i] > 0){
+                ans[a[i]] = true;
+            }
+        }
+        int re = -1;
+        for(int i=1; i<n;i++){
+            if(!ans[i]){
+                re = i;
+                break;
+            }
+        }
+        return re;
+    }
 }
--- src/SquaresSort.java
+++ src/SquaresSort.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class SquaresSort {
+import java.util.Arrays;
+
+public class SquaresSort {
+    public static void main(String[] args) {
+        int[] a = {-1, -3, 4};
+        int n = 6;
+        int[] b = {-9, -5, -3, 0 , 6, 8};
+        solve2(b, n);
+
+        solve1(a);
+        for(int i=0; i<a.length;i++){
+            System.out.print(a[i]);
+        }
+    }
+    public static int[] solve1(int[] a){
+        for(int i=0; i<a.length;i++){
+            a[i] = a[i]*a[i];
+        }
+        Arrays.sort(a);
+        return a;
+    }
+    public static void solve2(int[] a, int n){
+        int l=0, r=n-1;
+        int[] ans = new int[n];
+        for(int i=n-1;i >= 0; i--){
+            if(Math.abs(a[l])> a[r]){
+                ans[i] = a[l] * a[l];
+                l++;
+            }else{
+                ans[i] = a[r] * a[r];
+                r--;
+            }
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(ans[i] + " ");
+        }
+    }
 }
